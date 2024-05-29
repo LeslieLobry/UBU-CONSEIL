c95606369044f18b9c2d4d87c4f749a36cbee517
LeslieLobry <leslielobry@gmail.com>
2024-05-29 18:53:18 +0200

    presentation

--- src/components/Presentation/presentation.jsx
+++ src/components/Presentation/presentation.jsx
@@ -1,32 +1,30 @@
 import "./presentation.css"
 import seb from "../../assets/seb.png"
-import baniere from "../../assets/baniere.png"
+// import baniere from "../../assets/baniere.png"
 
 function Presentation (){
     return(
         <div className="presentation-contenant">
+            <h1 className="title">UBU CONSEILS</h1>
             <div className="presentation-photo">
                 <img src={seb} alt="sébastien lobry, herlies" />
             </div>
-            <div className="presentation-baniere">
+            <div className="presentation-elmt">
+                 {/* <div className="presentation-baniere">
                 <img src={baniere} alt="ubu conseils" />
 
-            </div>
+            </div> */}
             <div className="presentation-texte">
-                <p>Après 28 ans passés en tant que salariés dans un bureau de contrôle, il était temps pour nous de créer notre propre entreprise.
-
-Notre volonté, soutenue par une solide expérience professionnelle, est d’être à l’écoute de nos clients partenaires et de les accompagner dans la recherche de solutions adaptées et optimisées.
-
-Nous vous proposons une gamme de services destinés à la maîtrise d’ouvrage, incluant l’évaluation des risques professionnels liés aux bâtiments et l’accompagnement concernant vos obligations réglementaires.
-
-Nous offrons également une assistance technique pour la gestion du risque amiante et des équipements de travail tels que les EPI et les appareils de levage.
-
-Nous pouvons vous aider à améliorer la qualité de l’air intérieur en réalisant une auto-évaluation de votre établissement.
-
+                <p className="1">Après 28 ans passés en tant que salariés dans un bureau de contrôle, il était temps pour nous de créer notre propre entreprise.
+Notre volonté, soutenue par une solide expérience professionnelle, est d’être à l’écoute de nos clients partenaires et de les accompagner dans la recherche de solutions adaptées et optimisées.</p>
+<p className="2">Nous vous proposons une gamme de services destinés à la maîtrise d’ouvrage, incluant l’évaluation des risques professionnels liés aux bâtiments et l’accompagnement concernant vos obligations réglementaires.
+Nous offrons également une assistance technique pour la gestion du risque amiante et des équipements de travail tels que les EPI et les appareils de levage.</p>
+<p className="3">Nous pouvons vous aider à améliorer la qualité de l’air intérieur en réalisant une auto-évaluation de votre établissement.
 Enfin, nous sommes en mesure de réaliser votre document unique d’évaluation des risques.
-
 Basés en banlieue lilloise, nous intervenons dans l’ensemble des Hauts-de-France ainsi que sur le plan national.</p>
             </div>
+            </div>
+           
         </div>
     )
 }
--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,9 +1,21 @@
 import Form from "../../components/Form/form"
-
+import logo from "../../assets/logo.png"
+import "./contact.css"
 
 function Contact (){
     return(
-        <Form/>
+        <div className="contact-contenant">
+            <div className="contact-logo-coordonnee">
+                <div className="contact-logo"><img src={logo} alt="ubu conseils, herlies" /></div>
+            <div className="contact-coordonnee">
+                <h2>Sébastien Lobry</h2>
+                <p>06.03.18.19.27</p>
+                <p>sebastien.lobry@ubuconseils.fr</p>
+             </div> 
+            </div>
+            <Form/> 
+        </div>
+      
     )
 }
 export default Contact
