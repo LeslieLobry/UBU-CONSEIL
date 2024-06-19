ed57470392ff94558b769e29fe797e24fd6e4b23
LeslieLobry <leslielobry@gmail.com>
2024-06-19 15:47:47 +0200

    list

--- src/pages/Categorie/categorie.jsx
+++ src/pages/Categorie/categorie.jsx
@@ -3,10 +3,17 @@ import {useParams } from "react-router-dom";
 import Erreur from "../Erreur/erreur"
 import "./categorie.css"
 function Categorie (){
+    
     const { id } = useParams();
 const product = prestation.find((item) => item.id === id)
+const list = product?.details.map((details, list)=>
+    {
+     return <li key={list}>{details}</li>
+        })
+
 if (product === undefined){
 return(< Erreur />)
+
 }
 return(
     <div className="categorie-contenant">
@@ -14,17 +21,7 @@ return(
             <div className="categorie-elmt">
                 <div className="categorie-img"><img src={product.image} alt="ubu conseils" /></div>
             <div className="categorie-list">
-            <li>{product.details[0]}</li>
-            <li>{product.details[1]}</li>
-            <li>{product.details[2]}</li>
-            <li>{product.details[3]}</li>
-            <li>{product.details[4]}</li>
-            <li>{product.details[5]}</li>
-            <li>{product.details[6]}</li>
-            <li>{product.details[7]}</li>
-            <li>{product.details[8]}</li>
-            <li>{product.details[9]}</li>
-            <li>{product.details[10]}</li>            
+            {list}
             </div>
             </div>
             
